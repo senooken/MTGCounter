22c70b4486464421655d5ad8ba4198668b8ebeb6
SENOO, Ken <contact@senooken.jp>
2020-03-13 17:01:34 +0900

    MVP (Minimum Valueble Product)

--- app/src/main/java/jp/senooken/android/mtgcounter/MainActivity.java
+++ app/src/main/java/jp/senooken/android/mtgcounter/MainActivity.java
@@ -3,12 +3,38 @@ package jp.senooken.android.mtgcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int life1_ = 20;
+    private int life2_ = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlusButton1Click(View view) {
+        ++life1_;
+        TextView life = findViewById(R.id.life1);
+        life.setText(String.valueOf(life1_));
+    }
+    public void onMinusButton1Click(View view) {
+        --life1_;
+        TextView life = findViewById(R.id.life1);
+        life.setText(String.valueOf(life1_));
+    }
+
+    public void onPlusButton2Click(View view) {
+        ++life2_;
+        TextView life = findViewById(R.id.life2);
+        life.setText(String.valueOf(life2_));
+    }
+    public void onMinusButton2Click(View view) {
+        --life2_;
+        TextView life = findViewById(R.id.life2);
+        life.setText(String.valueOf(life2_));
     }
 }
