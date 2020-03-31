fd6210ede60ba8732a7430f931590fd899f95cda
SENOO, Ken <contact@senooken.jp>
2020-03-31 16:56:58 +0900

    Add back menu

--- app/src/main/java/jp/senooken/android/mtgcounter/AboutActivity.java
+++ app/src/main/java/jp/senooken/android/mtgcounter/AboutActivity.java
@@ -1,9 +1,10 @@
 package jp.senooken.android.mtgcounter;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -12,11 +13,23 @@ import java.util.HashMap;
 import java.util.List;
 
 public class AboutActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        // Show back menu.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         List<HashMap<String, String>> list = new ArrayList<>();
         HashMap<String, String> map;
